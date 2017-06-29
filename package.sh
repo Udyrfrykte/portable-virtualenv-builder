af301aeccb95edc93f3e4098ff1647a183634454
Udyrfrykte <Udyrfrykte@users.noreply.github.com>
2017-06-29 23:28:19 +0200

    output logs during packaging and stop when an error occurs

--- package.sh
+++ package.sh
@@ -1,21 +1,22 @@
 #! /bin/bash
+set -e
 
 cat >| requirements.txt
 
 # install build packages that may be needed for pip install
 if [ -n "$BUILD_PACKAGES" ]; then
-  yum install -y $BUILD_PACKAGES &>/dev/null
+  yum install -y $BUILD_PACKAGES >&2
 fi
 
 # create and activate venv
-virtualenv venv &>/dev/null
+virtualenv venv >&2
 source venv/bin/activate
 
 # install python packages in the virtual env
-pip install --requirement requirements.txt &>/dev/null
+pip install --requirement requirements.txt >&2
 
 # make the virtual env portable (and delete unsupported activate scripts)
-virtualenv --relocatable venv &>/dev/null
+virtualenv --relocatable venv >&2
 sed -i 's/VIRTUAL_ENV=.*/VIRTUAL_ENV="$( cd "$( dirname "${BASH_SOURCE[0]}" )\/.." \&\& pwd )"/' venv/bin/activate
 rm -f venv/bin/activate.*
 
